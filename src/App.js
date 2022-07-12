61d7bf09afd3047c6c0fe9ec4d637dae02aa1841
yildirimkadir <yildirimkad34@gmail.com>
2022-07-12 20:12:25 +0200

    "updated"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import "./Card.css";
+import { images } from './image';
+import "./App.css";
+import { useState } from 'react';
 
 
 function App() {
+  const [imgClick, setImgClick] = useState();
+
   return (
     <div className="App">
+      <h1>Düsseldorf Sehenswürdigkeiten</h1>
+      <div className="container">
+        <img src={imgClick} alt="selected" className="selected"/>
+      </div>
+      <div className="imgBox">
+        {images.map((item) => { 
+        const{name, img} = item;
+        return (
+          <img src={img} onClick={() => setImgClick(img)} alt="duesseldorf" />
+         )
+        })};
+      </div>
+
+
+    
     </div>
   );
 }
---
+++ src/image.js
@@ -0,0 +1,23 @@
+
+export const images = [
+  {
+    name: 'Rheinturm',
+    img: "https://mediafiles.reiseuhu.de/wp-content/uploads/2019/10/duesseldorf-rhein-rheinturm-1024x640.jpg",
+  },
+  {
+    name: 'Altstadt-Rathaus',
+    img: "https://mediafiles.reiseuhu.de/wp-content/uploads/2019/10/duesseldorf-altstadt-rathaus-1024x688.jpg",
+  },
+  {
+    name: 'Königsallee',
+    img: "https://mediafiles.reiseuhu.de/wp-content/uploads/2019/10/duesseldorf-koenigsallee-1024x683.jpg",
+  },
+  {
+    name: 'Hofgarten',
+    img: "https://mediafiles.reiseuhu.de/wp-content/uploads/2019/10/park-duesseldorf-1024x683.jpg",
+  },
+  {
+    name: 'Schloss und Park Benrath',
+    img: "https://mediafiles.reiseuhu.de/wp-content/uploads/2019/10/schloss-benrath-duesseldorf-1024x683.jpg",
+  },
+];
